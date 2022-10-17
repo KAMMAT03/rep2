1d1f54d03e1be528f6f8ea3ec12b8d5e5d906ccf
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2022-10-17 18:08:10 +0200

    start projektu

---
+++ w3_20Z_Git.java
@@ -0,0 +1,13 @@
+public class w3_20Z_Git {
+    public static void main(String[] args) {
+        int a = 10;
+        wypiszInt("Liczba a",a);
+        int b = 5;
+        wypiszInt("Liczba b",b);
+        int suma = a + b;
+        wypiszInt("Suma liczba a i b",suma);
+    }
+    private static void wypiszInt(String s, int a) {
+        System.out.println(s + ": " + a);
+    }
+}
