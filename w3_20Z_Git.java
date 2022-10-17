e541601978c7214200a0e0c337d9915d2aafa69b
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2022-10-17 21:23:27 +0200

    mod. metody wypisujacej i dekrementacja zm. c

--- w3_20Z_Git.java
+++ w3_20Z_Git.java
@@ -5,11 +5,13 @@ public class w3_20Z_Git {
         int b = 5;
         wypiszInt("Liczba b",b);
         int c = 15; //nowa instrukcja
+        c--;//modyfikacja zm. c <- modMain
         int d = 0; //nowa instrukcja <- modMain
         int suma = a + b;
         wypiszInt("Suma liczba a i b",suma);
     }
     private static void wypiszInt(String s, int a) {
         System.out.println(s + ": " + a);
+        System.out.println("Zrobione");//kasujemy <- modMain
     }
 }
